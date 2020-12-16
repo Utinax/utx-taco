7330d42261630e441065084502faa6184f73316f
Ali <74619504+Utinax@users.noreply.github.com>
2020-12-16 15:31:33 +0300

    Create disc.sql

---
+++ sql/disc.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('tacoeti', 'Taco Eti', 1, 0, 1),
+	('tacopaketi', 'Paketlenmiş Taco', 1, 0, 1),
+	('taco', 'Taco', 1, 0, 1);
