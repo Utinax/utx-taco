e97c68be650cb47164378989e3d0503b1d02aad9
Ali <74619504+Utinax@users.noreply.github.com>
2020-12-20 17:20:03 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,17 +5,59 @@ RegisterServerEvent('utx-taco:etver')
 AddEventHandler('utx-taco:etver', function()
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-    xPlayer.addInventoryItem('tacoeti', 1)
+    if xPlayer.canCarryItem('tacoeti', 1) then
+        xPlayer.addInventoryItem('tacoeti', 1)
+    else
+        TriggerClientEvent('esx:showNotification', src, 'Üzerinde bunu taşıyacak yer yok!')
+    end
+end)
+
+RegisterServerEvent('utx-taco:ekmekver')
+AddEventHandler('utx-taco:ekmekver', function()
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if xPlayer.canCarryItem('tacoekmegi', 1) then
+        xPlayer.addInventoryItem('tacoekmegi', 1)
+    else
+        TriggerClientEvent('esx:showNotification', src, 'Üzerinde bunu taşıyacak yer yok!')
+    end
+end)
+
+RegisterServerEvent('utx-taco:malzemever')
+AddEventHandler('utx-taco:malzemever', function()
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if xPlayer.canCarryItem('tacomalzemesi', 1) then
+        xPlayer.addInventoryItem('tacomalzemesi', 1)
+    else
+        TriggerClientEvent('esx:showNotification', src, 'Üzerinde bunu taşıyacak yer yok!')
+    end
 end)
 
 RegisterServerEvent('utx-taco:tacover')
 AddEventHandler('utx-taco:tacover', function()
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-    if xPlayer.getInventoryItem('tacoeti').count >= 3 then
-        xPlayer.removeInventoryItem('tacoeti', 3)
-        Citizen.Wait(500)
-        xPlayer.addInventoryItem('taco', 1)
+    if xPlayer.getInventoryItem('tacoeti').count >= 1 then
+        if xPlayer.getInventoryItem('tacoekmegi').count >= 1 then
+            if xPlayer.getInventoryItem('tacomalzemesi').count >= 1 then
+                if xPlayer.canCarryItem('taco', 1) then
+                    xPlayer.removeInventoryItem('tacoeti', 1)
+                    Citizen.Wait(250)
+                    xPlayer.removeInventoryItem('tacoekmegi', 1)
+                    Citizen.Wait(250)
+                    xPlayer.removeInventoryItem('tacomalzemesi', 1)
+                    Citizen.Wait(500)
+                    xPlayer.addInventoryItem('taco', 1)
+                else
+                    TriggerClientEvent('esx:showNotification', src, 'Üzerinde bunu taşıyacak yer yok!')
+                end
+            else
+                TriggerClientEvent('esx:showNotification', src, 'Üzerinde yeterli taco malzemesi yok!')
+            end
+        else
+            TriggerClientEvent('esx:showNotification', src, 'Üzerinde yeterli taco ekmeği yok!')
+        end
     end
 end)
 
@@ -24,9 +66,13 @@ AddEventHandler('utx-taco:paketver', function()
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
     if xPlayer.getInventoryItem('taco').count >= 1 then
-        xPlayer.removeInventoryItem('taco', 1)
-        Citizen.Wait(500)
-        xPlayer.addInventoryItem('tacopaketi', 1)
+        if xPlayer.canCarryItem('tacopaketi', 1) then
+            xPlayer.removeInventoryItem('taco', 1)
+            Citizen.Wait(500)
+            xPlayer.addInventoryItem('tacopaketi', 1)
+        else
+            TriggerClientEvent('esx:showNotification', src, 'Üzerinde bunu taşıyacak yer yok!')
+        end
     end
 end)
 
@@ -34,7 +80,7 @@ RegisterServerEvent('utx-taco:tacosat')
 AddEventHandler('utx-taco:tacosat', function()
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-    local money = math.random(25, 225)
+    local money = math.random(Config.MinParaMiktar, Config.MaxParaMiktar)
     if xPlayer.getInventoryItem('tacopaketi').count >= 1 then
         xPlayer.removeInventoryItem('tacopaketi', 1)
         Citizen.Wait(500)
@@ -44,11 +90,12 @@ end)
 
 ESX.RegisterServerCallback('utx-taco:malzemekontrol', function(source, cb, item, gereklisayi)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	itemcount = xPlayer.getInventoryItem(item).count
+    local itemcount = xPlayer.getInventoryItem(item).count
+    local itemname = xPlayer.getInventoryItem(item).label
 	if itemcount >= gereklisayi then
 		cb(true)
 	else
         activity = 0
-        TriggerClientEvent('esx:showNotification', source, 'Üzerinde yeterli malzeme yok!')
+        TriggerClientEvent('esx:showNotification', source, 'Üzerinde yeterli '..itemname..' yok!')
 	end
 end)
